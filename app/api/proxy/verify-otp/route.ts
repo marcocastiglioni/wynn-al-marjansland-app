702610ac9ae8b6c8fe018d8b3864e8ea5a150e52
Marco Castiglioni <marcocastiglioni@gmail.com>
2025-03-21 19:03:59 +0100

    Modifying endpoints urls

--- app/api/proxy/verify-otp/route.ts
+++ app/api/proxy/verify-otp/route.ts
@@ -4,7 +4,7 @@ export async function POST(request: Request) {
   try {
     const body = await request.json();
 
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}api/verify-otp`, {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/verify-otp`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
