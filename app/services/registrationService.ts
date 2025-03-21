8486c16f401bcf05cd5a50414543f8f6c0ebc788
Marco Castiglioni <marcocastiglioni@gmail.com>
2025-03-21 20:34:14 +0100

    Fixing User Registration service URL

--- app/services/registrationService.ts
+++ app/services/registrationService.ts
@@ -2,7 +2,7 @@ import { FormData } from '@/types/formData';
 import { RegistrationResponse } from '@/types/servicesData';
 
 export async function registerUser(data: FormData): Promise<RegistrationResponse> {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/register-user`, {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/register`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data),
